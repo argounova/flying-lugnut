2dcbf31c96f441a4791e9f826c75581e4aa7998d
Volantis <putzstuck@gmail.com>
2023-03-09 18:30:36 -0500

    edit to text on coming soon component

--- client/src/components/ComingSoon/component.jsx
+++ client/src/components/ComingSoon/component.jsx
@@ -7,7 +7,7 @@ const ComingSoon = () => {
         <div className="comingSoonDiv">
             <Stack className="d-flex align-items-center justify-content-center">
                 <p className="comingSoonTitle">Coming Soon</p>
-                <p className="comingSoonInfo">Bear with us as Flying Lugnut's new site is being built.  Check back often as updates are frequent!  In the meantime, please visit our <a style={{ textDecoration: 'none', color: '#F1D74D'}} href="http://flyinglugnut.etsy.com">Etsy shop</a>.</p>
+                <p className="comingSoonInfo">Bear with us as Flying Lugnut's new site is being built.  Browse our <a style={{ textDecoration: 'none', color: '#F1D74D'}} href='http://localhost:3000/browse-all'>current inventory</a> or visit our <a style={{ textDecoration: 'none', color: '#F1D74D'}} href="http://flyinglugnut.etsy.com">Etsy shop</a>.</p>
             </Stack>
         </div>
     );
